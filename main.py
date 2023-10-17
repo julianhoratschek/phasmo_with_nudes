d11252716fc423af5a920db1f45a0dcdb0d50cd7
Julian Horatschek <lando.horaz@gmail.com>
2023-10-17 19:51:34 +0200

    Moving Player with direction

--- main.py
+++ main.py
@@ -4,7 +4,46 @@ import arcade
 # TODO GameWindow/GameView
 
 
+from enum import Flag, auto
+
+
+class Direction(Flag):
+    Idle = 0
+    Up = auto()     # 0b0001
+    Down = auto()   # 0b0010
+    Left = auto()   # 0b0100
+    Right = auto()  # 0b1000 0b0111
+
+
+class Player(arcade.Sprite):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.direction: Direction = Direction.Idle
+        self.speed: float = 10.0
+
+    def on_update(self, delta_time: float = 1/60):
+        self.change_x, self.change_y = 0.0, 0.0
+
+        if Direction.Up in self.direction:
+            self.change_y += self.speed
+
+        if Direction.Down in self.direction:
+            self.change_y -= self.speed
+
+        if Direction.Right in self.direction:
+            self.change_x += self.speed
+
+        if Direction.Left in self.direction:
+            self.change_x -= self.speed
+
+
 class GameView(arcade.View):
+    DirectionKeys = {arcade.key.W: Direction.Up,
+                     arcade.key.S: Direction.Down,
+                     arcade.key.A: Direction.Left,
+                     arcade.key.D: Direction.Right}
+
     def __init__(self):
         super().__init__()
 
@@ -20,9 +59,8 @@ class GameView(arcade.View):
         self.ui_layer = arcade.SpriteList()
         self.scene.add_sprite_list(name="ui_layer", sprite_list=self.ui_layer)
 
-        self.player = arcade.Sprite("res/sprites/star.png")
+        self.player = Player(path_or_texture="res/sprites/star.png")
         self.scene.add_sprite(name="player", sprite=self.player)
-        self.player_speed = 10
 
     def on_draw(self):
         # TODO Draw everything
@@ -36,6 +74,7 @@ class GameView(arcade.View):
         # TODO NPC Interactions
         # TODO Player do move
         # TODO Camera movement
+        self.scene.on_update(delta_time)
         self.scene.update()
 
     def on_key_press(self, symbol: int, modifiers: int):
@@ -46,17 +85,8 @@ class GameView(arcade.View):
         # TODO Interaction? Taking Items? Place Items?
         # TODO Toggle Scrolling
 
-        if symbol == arcade.key.W:
-            self.player.change_y = self.player_speed
-
-        if symbol == arcade.key.A:
-            self.player.change_x = -self.player_speed
-
-        if symbol == arcade.key.S:
-            self.player.change_y = -self.player_speed
-
-        if symbol == arcade.key.D:
-            self.player.change_x = self.player_speed
+        if symbol in GameView.DirectionKeys:
+            self.player.direction |= GameView.DirectionKeys[symbol]
 
     def on_key_release(self, symbol: int, modifiers: int):
         # TODO Not running forever
