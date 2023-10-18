aced8e14cc083a20cc928b75e5d7b6e8175c2fe9
Julian Horatschek <lando.horaz@gmail.com>
2023-10-18 22:36:11 +0200

    LDTk Loader

--- main.py
+++ main.py
@@ -1,45 +1,11 @@
 import arcade
-from util.aseprite import Animation
+from characters import Player, Direction
 
+# TODO: Spawns on Integer Grid
+# TODO: Ghost selects goal/lifetime
+# TODO: Ghost Backstory, different backstories "patched"
 
-# TODO GameWindow/GameView
-
-
-from enum import Flag, auto
-
-
-class Direction(Flag):
-    Idle = 0
-    Up = auto()     # 0b0001
-    Down = auto()   # 0b0010
-    Left = auto()   # 0b0100
-    Right = auto()  # 0b1000 0b0111
-
-
-class Player(Animation):
-    def __init__(self, **kwargs):
-        super().__init__("res/sprites/dude.json", **kwargs)
-
-        self.direction: Direction = Direction.Idle
-        self.speed: float = 10.0
-
-    def on_update(self, delta_time: float = 1/60):
-        self.change_x, self.change_y = 0.0, 0.0
-
-        if Direction.Up in self.direction:
-            self.change_y += self.speed
-
-        if Direction.Down in self.direction:
-            self.change_y -= self.speed
-
-        if Direction.Right in self.direction:
-            self.change_x += self.speed
-
-        if Direction.Left in self.direction:
-            self.change_x -= self.speed
-
-        self.set_animation("walk" if any(self.velocity) else "idle")
-
+# TODO Furniture: Inventory, spawning backstory, more furniture
 
 class GameView(arcade.View):
     DirectionKeys = {arcade.key.W: Direction.Up,
---
+++ util/ldtk.py
@@ -0,0 +1,79 @@
+import arcade
+import json
+
+from pathlib import Path
+
+
+class Entity(arcade.Sprite):
+    def __init__(self, iid: str, **kwargs):
+        super().__init__(**kwargs)
+
+        self.entity_id: str = iid
+
+
+class Stairs(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.destination = None
+
+
+class Furniture(Entity):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.interactable: bool = False
+
+
+class TileMap:
+    LevelPath: Path = Path("res/maps/MapLayout/simplified")
+
+    def __init__(self):
+        self.tile_map: arcade.Texture | None = None
+        self.room_grid: list[list[int]] = []
+        self.room_names: list[str] = []
+
+        self.stairs: arcade.SpriteList = arcade.SpriteList()
+        self.furniture: arcade.SpriteList = arcade.SpriteList()
+
+    def load_level(self, level_nr: int):
+        lvl_path = TileMap.LevelPath / f"Level_{level_nr}"
+        self.tile_map = arcade.load_texture(lvl_path / "_composite.png")
+
+        with open(lvl_path / "Rooms.csv", "r") as rooms_file:
+            for line in rooms_file.readlines():
+                self.room_grid.append([int(x) for x in line.split(",")])
+
+        with open("res/maps/MapLayout.ldtk", "r") as map_file:
+            json_data = json.load(map_file)
+            for layer in json_data["defs"]["layers"]:
+                if layer["identifier"] == "Rooms":
+                    for room_name in layer["intGridValues"]:
+                        self.room_names.append(room_name["identifier"])
+                    break
+
+        with open(lvl_path / "data.json", "r") as entity_file:
+            entity_json = json.load(entity_file)
+
+            for furniture in entity_json["entities"]["Furniture"]:
+                new_furniture = Furniture(
+                    iid=furniture["iid"],
+                    center_x=furniture["x"],
+                    center_y=furniture["y"])
+                new_furniture.interactable = furniture["customFields"]["Interactable"]
+                self.furniture.append(new_furniture)
+
+            stairs_dict: dict[str:tuple[Stairs, str]] = dict()
+
+            for stairs in entity_json["entities"]["Stair"]:
+                stairs_dict[stairs["iid"]] = (Stairs(iid=stairs["iid"], center_x=stairs["x"], center_y=stairs["y"]),
+                                              stairs["destination"])
+
+            for stair_obj, dest in stairs_dict.values():
+                stair_obj.destination = stairs_dict[dest][0]
+                self.stairs.append(stair_obj)
+        pass
+
+
+
+
