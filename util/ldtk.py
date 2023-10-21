809b5ab96e07e2df57b1b5e2b54947c9621a646a
Julian Horatschek <lando.horaz@gmail.com>
2023-10-21 10:29:08 +0200

    Correct Idents to close files after loading
    TODOs added to Player- and NPC import

--- util/ldtk.py
+++ util/ldtk.py
@@ -50,48 +50,49 @@ class TileMap:
 
         with open("res/maps/MapLayout.ldtk", "r") as map_file:
             json_data = json.load(map_file)
-            for layer in json_data["defs"]["layers"]:
-                if layer["identifier"] == "Rooms":
-                    for room_name in layer["intGridValues"]:
-                        self.room_names.append(room_name["identifier"])
-                    break
+
+        for layer in json_data["defs"]["layers"]:
+            if layer["identifier"] == "Rooms":
+                for room_name in layer["intGridValues"]:
+                    self.room_names.append(room_name["identifier"])
+                break
 
         with open(lvl_path / "data.json", "r") as entity_file:
             entity_json = json.load(entity_file)
 
-            for furniture in entity_json["entities"]["Furniture"]:
-                new_furniture = Furniture(
-                    iid=furniture["iid"],
-                    center_x=furniture["x"],
-                    center_y=furniture["y"])
-                new_furniture.interactable = furniture["customFields"]["Interactable"]
-                self.furniture.append(new_furniture)
+        for furniture in entity_json["entities"]["Furniture"]:
+            new_furniture = Furniture(
+                iid=furniture["iid"],
+                center_x=furniture["x"],
+                center_y=furniture["y"])
+            new_furniture.interactable = furniture["customFields"]["Interactable"]
+            self.furniture.append(new_furniture)
 
-            stairs_dict: dict[str:tuple[Stairs, str]] = dict()
+        stairs_dict: dict[str:tuple[Stairs, str]] = dict()
 
-            for stairs in entity_json["entities"]["Stair"]:
-                stairs_dict[stairs["iid"]] = (Stairs(iid=stairs["iid"], center_x=stairs["x"], center_y=stairs["y"]),
-                                              stairs["destination"])
+        for stairs in entity_json["entities"]["Stair"]:
+            stairs_dict[stairs["iid"]] = (Stairs(iid=stairs["iid"], center_x=stairs["x"], center_y=stairs["y"]),
+                                          stairs["destination"])
 
-            for stair_obj, dest in stairs_dict.values():
-                stair_obj.destination = stairs_dict[dest][0]
-                self.stairs.append(stair_obj)
+        for stair_obj, dest in stairs_dict.values():
+            stair_obj.destination = stairs_dict[dest][0]
+            self.stairs.append(stair_obj)
 
-            #NEW!
-            #Adds the initial Player and NPC Positions as SpriteLists with Entity Types
-            for npc in entity_json["entities"]["NPC"]:
-                self.npc_pos.append(Entity(
-                    iid=npc["iid"],
-                    center_x=npc["x"],
-                    center_y=npc["y"]))
+        for npc in entity_json["entities"]["NPC"]:
+            # TODO Which NPC stands where? Part of Backstory-Class?
+            self.npc_pos.append(Entity(
+                iid=npc["iid"],
+                center_x=npc["x"],
+                center_y=npc["y"]))
 
-            player = entity_json["entities"]["Player"][0]
-            self.player_pos.append(Entity(
-                iid=player["iid"],
-                center_x=player["x"],
-                center_y=player["y"]))
-
-        pass
+        # TODO: Do we need a Player-Entity?
+        #  Why in a Spritelist? How may Player-entities are possible per map?
+        #  We have a Player-Class. What Information do we need from ldtk?
+        player = entity_json["entities"]["Player"][0]
+        self.player_pos.append(Entity(
+            iid=player["iid"],
+            center_x=player["x"],
+            center_y=player["y"]))
 
 
 
