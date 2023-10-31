8176556314c3ed4a36179beae32a09662962dffc
Julian Horatschek <lando.horaz@gmail.com>
2023-10-31 17:38:57 +0100

    Less Qualifiers

--- shaders/flashlight.glsl
+++ shaders/flashlight.glsl
@@ -1,10 +1,3 @@
-uniform const vec2 ScreenCenter = vec2(0.5, 0.5);
-uniform const float Cone = 6.0;
-uniform const float LengthModificator = 1.4;
-
-uniform const int ShadowSamples = 50;
-uniform const float ShadowStep = 0.008;
-
 float rand() {
     return fract(sin(iTime) * 100);
 }
@@ -14,18 +7,18 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 uv = fragCoord / iResolution.xy;
     vec2 mouse_uv = iMouse.xy / iResolution.xy;
 
-    vec2 center = uv - ScreenCenter;
-    vec2 ray_direction = mouse_uv - ScreenCenter;
+    vec2 center = uv - vec2(0.5, 0.5);
+    vec2 ray_direction = mouse_uv - vec2(0.5, 0.5);
 
-    float power = pow(dot(normalize(center), normalize(ray_direction)), Cone);
-    float light = mix(0.2, power, 1 - length(center) * LengthModificator);
+    float power = pow(dot(normalize(center), normalize(ray_direction)), 6.0);
+    float light = mix(0.2, power, 1 - length(center) * 1.4);
 
     float shadow = 0.0;
     if(power > 0 && texture(iChannel0, uv).a == 0) {
-        for (int i = 0; i < ShadowSamples; i++) {
-            vec2 pv = uv - (i * ShadowStep * normalize(ray_direction));
-            if(dot(normalize(ray_direction), normalize(pv - ScreenCenter)) > 0)
-                shadow += texture(iChannel0, pv).a / ShadowSamples;
+        for (int i = 0; i < 50; i++) {
+            vec2 pv = uv - (i * 0.008 * normalize(ray_direction));
+            if(dot(normalize(ray_direction), normalize(pv - vec2(0.5, 0.5))) > 0)
+                shadow += texture(iChannel0, pv).a / 50;
         }
     }
 
